7f0496984934ad5cfcd065e14d0b76aa01584154
ISSAM LAOUMRI <issamlaoumri@gmail.com>
2025-08-23 17:08:21 +0000

    feat: setup a fully functional routing & navigation system

---
+++ app/movies/[id].tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import {Text, View} from 'react-native';
+
+const MovieDetails = () => {
+    return (
+        <View>
+            <Text>
+                MovieDetails
+            </Text>
+        </View>
+    );
+};
+
+export default MovieDetails;
